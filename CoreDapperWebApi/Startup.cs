0b10639e9113f7e73b1a5b29c2be24a98555ac89
8023ljq <809395347@qq.com>
2020-03-06 18:03:04 +0800

    修改

--- CoreDapperWebApi/Startup.cs
+++ CoreDapperWebApi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreDapperWebApi.HubHelp;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -51,6 +52,20 @@ namespace CoreDapperWebApi
                 // 如字段为null值，该字段不会返回到前端
                 // options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
             });
+
+            //注入SignalR实时通讯，默认用json传输
+            services.AddSignalR(options =>
+            {
+                //客户端发保持连接请求到服务端最长间隔，默认30秒，改成4分钟，网页需跟着设置connection.keepAliveIntervalInMilliseconds = 12e4;即2分钟
+                options.ClientTimeoutInterval = TimeSpan.FromMinutes(4);
+                //服务端发保持连接请求到客户端间隔，默认15秒，改成2分钟，网页需跟着设置connection.serverTimeoutInMilliseconds = 24e4;即4分钟
+                options.KeepAliveInterval = TimeSpan.FromMinutes(2);
+            });
+
+            //注入跨域
+            services.AddCors(option => option.AddPolicy("cors",
+                policy => policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+                    .WithOrigins("http://localhost:8001", "http://localhost:8000", "http://localhost:8002")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,6 +89,22 @@ namespace CoreDapperWebApi
             {
                 endpoints.MapControllers();
             });
+
+             //添加WebSocket支持，SignalR优先使用WebSocket传输
+            app.UseWebSockets();
+            //app.UseWebSockets(new WebSocketOptions
+            //{
+            //    //发送保持连接请求的时间间隔，默认2分钟
+            //    KeepAliveInterval = TimeSpan.FromMinutes(2)
+            //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<MessageHub>("/messageHub");
+            });
+
+            //允许跨域，不支持向所有域名开放了，会有错误提示
+            app.UseCors("cors");
         }
     }
 }
