7df4b4581a9bc190564ddc324123afaa21f54dbf
8023ljq <809395347@qq.com>
2020-04-17 15:06:05 +0800

    修改

--- CoreDapperWebApi/Controllers/CommonController/LoginController.cs
+++ CoreDapperWebApi/Controllers/CommonController/LoginController.cs
@@ -7,6 +7,7 @@ using CoreDapperCommon.CommonMethod;
 using CoreDapperModel.CommonModel;
 using CoreDapperModel.ViewModel;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
--- CoreDapperWebApi/HubHelp/MessageHub.cs
+++ CoreDapperWebApi/HubHelp/MessageHub.cs
@@ -3,11 +3,22 @@ using System.Threading.Tasks;
 
 namespace CoreDapperWebApi.HubHelp
 {
-    public class MessageHub : Hub
+    public class MessageHub : Hub<IChatClient>
     {
-         public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.ReceiveMessage(user, message);
+        }
+
+        //定于一个通讯管道，用来管理我们和客户端的连接
+        //1、客户端调用 GetLatestCount，就像订阅
+        public async Task GetLatestCount(string random)
+        {
+            //2、服务端主动向客户端发送数据，名字千万不能错
+            await Clients.All.ReceiveUpdate("54154653");
+
+            //3、客户端再通过 ReceiveUpdate ，来接收
+
         }
     }
 }
